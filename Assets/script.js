7ffb8b089b80743821a40d9e4acaa5623b5f30d2
Zane Werle <zanewerle@gmail.com>
2023-03-21 17:45:26 -0500

    added in code that asks you how long the password should be. Currently there is no defined number, but I added code that shows an alert message if you try adding something that is not a number

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,3 @@
-var specialCharacters = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', ];
-
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',];
-
-var lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
-
-var upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -21,3 +12,38 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var specialCharacters = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', ];
+
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',];
+
+var lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
+
+var upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+
+function generatePassword(){
+  var passwordLength = parseInt(
+    prompt("How long would you like your password to be?")
+  );
+
+  if (Number.isNaN(passwordLength)) {
+    alert("Must provide a number for the password length.");
+    return null;
+  }
+
+  if (passwordLength < 8) {
+    alert("dont be dumb son");
+    return null;
+  }
+  
+
+
+
+
+
+
+
+
+}
+
+
